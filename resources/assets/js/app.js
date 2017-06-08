a7bf407c89842dc54f4a80c85b57f168a31ce268
mnv <mnvx@yandex.ru>
2017-06-08 11:05:09 +0300

    leaflet example

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -7,16 +7,33 @@
 
 require('./bootstrap');
 
-window.Vue = require('vue');
+let L = require('leaflet');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+let tiliktinoLatLon = [56.0858, 36.612];
+let zoom = 17;
+let mymap = L.map('map').setView(tiliktinoLatLon, zoom);
+let osmTilesUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+let memberStyle = {
+    color: 'green',
+    stroke: false,
+};
 
-Vue.component('example', require('./components/Example.vue'));
+L.tileLayer(osmTilesUrl, {
+    maxZoom: 19,
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+}).addTo(mymap);
 
-const app = new Vue({
-    el: '#app'
-});
+L.polygon([
+    [56.0874326, 36.6100406],
+    [56.0879243, 36.6099735],
+    [56.0880779, 36.6103181],
+    [56.0874442, 36.6104105]
+], memberStyle).addTo(mymap).bindPopup(["Участок 1", "hello world"].join('<br>'));
+
+L.polygon([
+    [56.0875042, 36.6118597],
+    [56.0875145, 36.6120588],
+    [56.0875981, 36.6121368],
+    [56.0881112, 36.6120410],
+    [56.0881584, 36.6117440]
+], memberStyle).addTo(mymap).bindPopup(["Участок 6", "Something is happend!"].join('<br>'));
